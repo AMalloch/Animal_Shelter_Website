b27a68e4e5f272ae04ea920084aea27e16847dd1
Angus Malloch <angusbass@hotmail.com>
2019-02-17 13:08:47 +0000

    Tidy runner code.

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,16 +1,14 @@
 require('pg')
 
 class SqlRunner
-
   def self.run(sql, values = [])
     begin
-      db = PG.connect({ dbname: 'shelter', host: 'localhost' })
-      db.prepare("query", sql)
-      result = db.exec_prepared("query", values)
+      db = PG.connect(dbname: 'shelter', host: 'localhost')
+      db.prepare('query', sql)
+      result = db.exec_prepared('query', values)
     ensure
-      db.close() if db != nil
+      db.close if db != nil
     end
-    return result
+    result
   end
-
 end
