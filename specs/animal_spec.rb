6ff48ba0ee3e4b3bb3bd4bcb2561655480210c96
Angus Malloch <angusbass@hotmail.com>
2019-02-17 13:14:28 +0000

    Tidy test code.

--- specs/animal_spec.rb
+++ specs/animal_spec.rb
@@ -1,19 +1,17 @@
-require("minitest/autorun")
-require("minitest/rg")
+require('minitest/autorun')
+require('minitest/rg')
 
-require_relative("../models/animal.rb")
-require_relative("../models/owner.rb")
+require_relative('../models/animal.rb')
+require_relative('../models/owner.rb')
 
-class Animal_Test < MiniTest::Test
-
-  def test_adopt_status_is_boolean
+class AnimalTest < MiniTest::Test
+  def test_adopt_status_is_true
     status = 't'
-    assert_equal("True", @animal.status_to_b(status))
+    assert_equal('True', @animal.status_to_b(status))
   end
 
-  def test_adopt_status_is_boolean
+  def test_adopt_status_is_false
     status = 'f'
-    assert_equal("False", @animal.status_to_b(status))
+    assert_equal('False', @animal.status_to_b(status))
   end
-
 end
